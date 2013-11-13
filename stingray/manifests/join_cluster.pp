765606bfc7329e1213b45b16175c313c09e9e6ad
fmemonrvbd <fmemon@riverbed.com>
2013-11-12 17:38:10 -0800

    Remove some commented out code

--- stingray/manifests/join_cluster.pp
+++ stingray/manifests/join_cluster.pp
@@ -19,16 +19,11 @@
 # [*admin_password*]
 # The administrator password of the cluster.  Defaults to 'password'.
 #
-# [*license_key*]
-# Path to the license key file. Providing no license key file, defaults to
-# developer mode.
-#
 # === Examples
 #
 #  stingray::join_cluster { 'my_cluster':
 #      join_cluster_host => 'The other STM',
 #      admin_password    => 'my_password',
-#      license_key       => 'puppet:///modules/stingray/license.txt
 #  }
 #
 # === Authors
@@ -44,23 +39,12 @@ define stingray::join_cluster (
     $join_cluster_port = '9090',
     $admin_username = $stingray::params::admin_username,
     $admin_password = $stingray::params::admin_password,
-    #$license_key = $stingray::params::license_key
 ) {
     include stingray
     include stingray::params
 
     $path = $stingray::install_dir
     $accept_license = $stingray::accept_license
-
-    #if ($license_key != '') {
-    #    file { "${path}/license.txt":
-    #        source  => $license_key,
-    #        before  => [ File['join_stingray_cluster_replay'], ],
-    #        require => [ Exec['install_stingray'], ],
-    #        alias   => 'join_cluster_stingray_license',
-    #    }
-    #    $local_license_key = "${path}/license.txt"
-    #}
 
     file { "${path}/join_cluster_replay":
         content => template ('stingray/join_cluster.erb'),
@@ -75,19 +59,4 @@ define stingray::join_cluster (
         alias   => 'join_stingray_cluster',
         creates => "${path}/rc.d/S10admin",
     }
-
-    #if ($license_key == '') {
-    #    if ($::fqdn) {
-    #        $host = $::fqdn
-    #    } else {
-    #        $host = $::hostname
-    #    }
-
-    #    file_line { 'developer_license_accepted':
-    #        ensure  => present,
-    #        path    => "${path}/zxtm/conf/zxtms/${host}",
-    #        line    => 'developer_mode_accepted yes',
-    #        require => [ Exec['join_stingray_cluster'], ],
-    #    }
-    #}
 }
