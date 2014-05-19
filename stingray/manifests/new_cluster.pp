a410b60b3060b86ac28f777538ec36597e17954f
Andre timmermann <andre.timmermann@local.ch>
2014-05-19 16:02:57 +0200

    add --noninteractive to new cluster to stop asking questions

--- stingray/manifests/new_cluster.pp
+++ stingray/manifests/new_cluster.pp
@@ -33,86 +33,86 @@
 # Copyright 2013 Riverbed Technology
 #
 define stingray::new_cluster (
-    $admin_password = $stingray::params::admin_password,
-    $license_key = $stingray::params::license_key
+  $admin_password = $stingray::params::admin_password,
+  $license_key = $stingray::params::license_key
 
 ) {
-    include stingray
-    include stingray::params
+  include stingray
+  include stingray::params
 
-    $path = $stingray::install_dir
-    $accept_license = $stingray::accept_license
+  $path = $stingray::install_dir
+  $accept_license = $stingray::accept_license
 
 
-    file { "${path}/new_cluster_replay":
-        content => template ('stingray/new_cluster.erb'),
-        require => [ Exec['install_stingray'], ],
-        alias   => 'new_stingray_cluster_replay',
-    }
+  file { "${path}/new_cluster_replay":
+    content => template ('stingray/new_cluster.erb'),
+    require => [ Exec['install_stingray'], ],
+    alias   => 'new_stingray_cluster_replay',
+  }
 
-    exec { "${path}/zxtm/configure --replay-from=new_cluster_replay":
-        cwd     => $path,
-        user    => 'root',
-        require => [ File['new_stingray_cluster_replay'], ],
-        alias   => 'new_stingray_cluster',
-        creates => "${path}/rc.d/S10admin",
-    }
+  exec { "${path}/zxtm/configure --replay-from=new_cluster_replay --noninteractive":
+    cwd     => $path,
+    user    => 'root',
+    require => [ File['new_stingray_cluster_replay'], ],
+    alias   => 'new_stingray_cluster',
+    creates => "${path}/rc.d/S10admin",
+  }
 
-    if ($license_key == '') {
-        if ($::fqdn) {
-            $host = $::fqdn
-        } else {
-            $host = $::hostname
-        }
+  if ($license_key == '') {
+    if ($::fqdn) {
+      $host = $::fqdn
+      } else {
+        $host = $::hostname
+      }
 
-        file_line { 'developer_license_accepted':
-            ensure  => present,
-            path    => "${path}/zxtm/conf/zxtms/${host}",
-            line    => 'developer_mode_accepted yes',
-            require => [ Exec['new_stingray_cluster'], ],
-        }
+      file_line { 'developer_license_accepted':
+        ensure  => present,
+        path    => "${path}/zxtm/conf/zxtms/${host}",
+        line    => 'developer_mode_accepted yes',
+        require => [ Exec['new_stingray_cluster'], ],
+      }
 
+      file { "${path}/zxtm/conf/licensekeys/license.txt":
+        ensure  => absent,
+        notify  => Exec['replicate_config'],
+        require => [ Exec['new_stingray_cluster'], ],
+      }
+      } else {
         file { "${path}/zxtm/conf/licensekeys/license.txt":
-            ensure  => absent,
-            notify  => Exec['replicate_config'],
-            require => [ Exec['new_stingray_cluster'], ],
+          ensure  => present,
+          source  => $license_key,
+          require => [ Exec['new_stingray_cluster'], ],
+          alias   => 'new_stingray_cluster_license',
+          notify  => Exec['replicate_config']
         }
-    } else {
-        file { "${path}/zxtm/conf/licensekeys/license.txt":
-            ensure  => present,
-            source  => $license_key,
-            require => [ Exec['new_stingray_cluster'], ],
-            alias   => 'new_stingray_cluster_license',
-            notify  => Exec['replicate_config']
-        }
-    }
+      }
 
-    file {"${path}/zxtm/bin/puppet_tip.sh":
+      file {"${path}/zxtm/bin/puppet_tip.sh":
         mode    => '0755',
         source  => 'puppet:///modules/stingray/puppet_tip.sh',
         require => [ Exec['new_stingray_cluster'], ]
-    }
+      }
 
-    # Manage users and groups
-    concat { "${path}/zxtm/conf/users":
+      # Manage users and groups
+      concat { "${path}/zxtm/conf/users":
         require => Exec['new_stingray_cluster'],
         notify  => Exec['replicate_config']
-    }
+      }
 
-    local_user { 'admin':
+      local_user { 'admin':
         password => $admin_password,
         clear_pw => 'yes',
         require  => [ Exec['new_stingray_cluster'], ]
-    }
+      }
 
-    file { "${path}/zxtm/conf/ssl/server_keys_config":
+      file { "${path}/zxtm/conf/ssl/server_keys_config":
         ensure  => 'present',
         alias   => 'server_keys_config',
         require => [ Exec['new_stingray_cluster'], ],
-    }
+      }
 
 
-    stingray::del_unused_resources{'del_unused_resources':
+      stingray::del_unused_resources{'del_unused_resources':
         require => [ Exec['new_stingray_cluster'], ]
-    }
+      }
 }
