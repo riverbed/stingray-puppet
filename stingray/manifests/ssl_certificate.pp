75adec345ee8954ecda61abe6a00c069fe906d73
Peter Tornyi <peter.tornyi@piksel.com>
2015-05-18 12:46:30 +0100

    Adding dependencies

--- stingray/manifests/ssl_certificate.pp
+++ stingray/manifests/ssl_certificate.pp
@@ -33,54 +33,87 @@
 #
 define stingray::ssl_certificate(
     $certificate_file = '',
-    $private_key_file = ''
-
+    $private_key_file = '',
+    $request_file = '',
+    $createdvia = 'software',
+    $protection = 'software',
+    $note = 'Added via puppet'
 ) {
     include stingray
 
     $path = $stingray::install_dir
 
     info ("Configuring SSL certificate ${name}")
+    # Upload ssl key files
     file { "${path}/zxtm/conf/ssl/server_keys/${name}.public":
         ensure => 'present',
         source => $certificate_file,
-        notify => Exec['replicate_config']
+        alias  => 'stingray_public_key',
+        notify => Exec['replicate_config'],
+        require => Exec['new_stingray_cluster']
     }
-
     file { "${path}/zxtm/conf/ssl/server_keys/${name}.private":
         ensure => 'present',
         source => $private_key_file,
-        notify => Exec['replicate_config']
+        notify => Exec['replicate_config'],
+        alias  => 'stingray_private_key',
+        require => Exec['new_stingray_cluster']
     }
-
-#    file { "${path}/zxtm/conf/ssl/server_keys_config":
-#        ensure  => 'present',
-#        alias   => 'server_keys_config',
-#        require => [ Exec['new_stingray_cluster'], ],
-#        notify  => Exec['replicate_config']
-#    }
+    file { "${path}/zxtm/conf/ssl/server_keys/${name}.request":
+        ensure => 'present',
+        source => $request_file,
+        notify => Exec['replicate_config'],
+        alias  => 'stingray_request_key',
+        require => Exec['new_stingray_cluster']
+    }
 
     file_line { "public key.${name}":
         ensure  => present,
         path    => "${path}/zxtm/conf/ssl/server_keys_config",
-        line    => "${name}!public  ${path}/zxtm/conf/ssl/server_keys/${name}.public",
-        require => File['server_keys_config'],
+        line    => "${name}!public %zeushome%/conf/ssl/server_keys/${name}.public",
+        require => File['stingray_public_key'],
         notify  => Exec['replicate_config']
-    }
-
+    } ->
     file_line { "private key.${name}":
         ensure  => present,
         path    => "${path}/zxtm/conf/ssl/server_keys_config",
-        line    => "${name}!private  ${path}/zxtm/conf/ssl/server_keys/${name}.private",
-        require => File['server_keys_config'],
+        line    => "${name}!private %zeushome%/zxtm/conf/ssl/server_keys/${name}.private",
+        require => File['stingray_private_key'],
         notify  => Exec['replicate_config']
-    }
-
+    } ->
+    file_line { "request file.${name}":
+        ensure  => present,
+        path    => "${path}/zxtm/conf/ssl/server_keys_config",
+        line    => "${name}!request %zeushome%/zxtm/conf/ssl/server_keys/${name}.request",
+        require => File['stingray_private_key'],
+        notify  => Exec['replicate_config']
+    } ->
+    file_line { "createdvia.${name}":
+        ensure  => present,
+        path    => "${path}/zxtm/conf/ssl/server_keys_config",
+        line    => "${name}!createdvia ${createdvia}",
+        require => File['stingray_private_key'],
+        notify  => Exec['replicate_config']
+    } ->
+    file_line { "protection.${name}":
+        ensure  => present,
+        path    => "${path}/zxtm/conf/ssl/server_keys_config",
+        line    => "${name}!protection ${protection}",
+        require => File['stingray_private_key'],
+        notify  => Exec['replicate_config']
+    } ->
     file_line { "managed.${name}":
         ensure  => present,
         path    => "${path}/zxtm/conf/ssl/server_keys_config",
-        line    => "${name}!managed  yes",
-        require => File['server_keys_config'],
+        line    => "${name}!managed yes",
+        require => File['stingray_private_key'],
+        notify  => Exec['replicate_config']
+    } ->
+    file_line { "note.${name}":
+        ensure  => present,
+        path    => "${path}/zxtm/conf/ssl/server_keys_config",
+        line    => "${name}!note ${note}",
+        require => File['stingray_private_key'],
         notify  => Exec['replicate_config']
     }
 }
